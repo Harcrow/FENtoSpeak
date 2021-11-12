bdace0e0ae36d767553887a73bc9a0c91c07a0f2
Tyler Jowaisas <tjowaisas@evolv.lan>
2021-11-11 19:52:32 -0500

    gotten to the point where a single FEN as a variable in the file works to generate an MP3 that rattles off the current position.  I'm not trying to read in from a file multiple FEN and doing it several times in a row.

--- FENtoSpeak.py
+++ FENtoSpeak.py
@@ -1,19 +1,20 @@
-import pyttsx3
+#import pyttsx3
 import chess
 from gtts import gTTS
 from pydub import AudioSegment
+import os
 
+path = 'C:\\Users\\tjowaisas\\Documents\\Hobby\\Chess\\FEN to speak\\Audio Files\\'
+fenSource = 'C:\\Users\\tjowaisas\\Documents\\Hobby\\Chess\\FEN to speak\\FEN.txt'
 
-test = "regular text speed no pause regular text speed comma pause, regular text speed period pause. regular text speed exclamation pause! regular text speed ellipses pause... regular text speed new line pause \n regular text speed "
-
-board_string = " "
-    
-
+readFen = open(fenSource)
+fenList = readFen.readlines()
+print(fenList)
 #initialize the test to speak engine. 
 #engine = pyttsx3.init()
 
+
 #the FEN for our starting position.  This is a basic mate in 1
-testfen = '1k6/8/1K6/8/4Q3/8/8/8 w - - 0 1'
 board = chess.Board()
 board.set_fen(testfen)
 
@@ -25,22 +26,26 @@ board.set_fen(testfen)
 # engine.setProperty('rate', speak_rate)
 # engine.setProperty('voice', Zira)
 
+#Generating the side announcements
 tts_white = gTTS(text="...White's Position...", lang='en')
-tts_white.save("whitePrompt.mp3")
+tts_white.save(path+"whitePrompt.mp3")
 
 tts_black = gTTS(text="...Black's Position...", lang='en')
-tts_black.save("blackPrompt.mp3")
+tts_black.save(path+"blackPrompt.mp3")
 
-whiteIntro = AudioSegment.from_file(r"whitePrompt.mp3")
-blackIntro = AudioSegment.from_file(r"blackPrompt.mp3")
+#reading the annoucements into memory
+whiteIntro = AudioSegment.from_file(path+"whitePrompt.mp3")
+blackIntro = AudioSegment.from_file(path+"blackPrompt.mp3")
 
-delay_one = AudioSegment.from_file(r"silence1.mp3")
-delay_five = AudioSegment.from_file(r"silence5.mp3")
+#generating some delays for a little configurability.  500mS and 2S delays
+delay_one = AudioSegment.from_file(path+"silence1.mp3")
+delay_five = AudioSegment.from_file(path+"silence5.mp3")
 delay_two = delay_one + delay_one
 delay_half = delay_one[-500:]
 
-WhiteHeader = whiteIntro + delay_two
-BlackHeader = blackIntro + delay_two 
+#side annoucement + 2 seconds
+WhitePositions = whiteIntro + delay_one
+BlackPositions= blackIntro + delay_one
 
 #setting up lists.
 """white_pieces= []
@@ -50,13 +55,15 @@ black_pieces = []
 black_squares = []
 """
 
-white_position = []
-black_position = []
+#white_position = []
+#black_position = []
 
-white_string = " "
-black_string = " " 
+#white_string = " "
+#black_string = " " 
 
 #This loops across every square and if there is a piece, it determines the color, type and square.  It appends that information to different lists and is stored.  This is basically sorting the information so that it will be read off as 'all of white's pieces' and then 'all of blacks pieces'
+
+#This *used* to make lists.  Due to needed flexibility in delays in reading off piece positions, I had to break everything into it's own MP3 that will later be concatenated.  
 for i in chess.SQUARES:
     piece = board.piece_at(i)
     square = chess.square_name(i)
@@ -71,7 +78,7 @@ for i in chess.SQUARES:
             #white_position.append(" on ")
             #white_position.append(square)
             tts = gTTS(str(TypeofPiece) + " on " + str(square), lang='en')
-            tts.save("White" + str(TypeofPiece)+"_"+ str(square) + ".mp3")
+            tts.save(path + "White_" + str(TypeofPiece)+"_"+ str(square) + ".mp3")
                         
         else:        
             piece_color = "Black"  
@@ -79,15 +86,35 @@ for i in chess.SQUARES:
             #black_position.append(" on ")
             #black_position.append(square)
             tts = gTTS(str(TypeofPiece) + " on " + str(square), lang='en')
-            tts.save("Black" + str(TypeofPiece)+"_"+ str(square) + ".mp3")
+            tts.save(path + "Black_" + str(TypeofPiece)+"_"+ str(square) + ".mp3")
     else:
         None
+
+#generating a list of files that are white or black positions
+print("Looking for position mp3 files...")
+for root, dirs, files in os.walk(path): 
+    for file in files:
+        if file.startswith("White_"):
         
+            print("Found file " + file)
+            temp = AudioSegment.from_file(path+file)
+            WhitePositions = WhitePositions + temp + delay_half
+            
+        elif file.startswith("Black_"):
         
+            print("Found file " + file)
+            temp = AudioSegment.from_file(path+file)
+            BlackPositions = BlackPositions + temp + delay_half
+            
+        else:
+            print("Bro what is this " + str(file))
+
+FinalPosition = WhitePositions + BlackPositions
+
+FinalPosition.export("Final_Fen.mp3", format="mp3")
+
 #pyttsx3.speak("Whites Position")
 #board_position.append("White's Position. ...")
-
-
 
 """for i in range(len(white_pieces)):
    # engine.say(white_pieces[i] +" on "+ white_squares[i])
